a999dd669d3f1661f396db44d948b8cec8d3e321
Daniel-Rauh <daniel.rauh@gmail.com>
2020-07-20 13:57:37 +0200

    v1.0

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+const four = document.getElementById("four")
+const five = document.getElementById("five")
+const six = document.getElementById("six")
+const custom = document.getElementById("custom")
+const customRounds = document.getElementById("customRounds")
+const guessed = document.getElementById("guess")
+const hints = document.getElementById("hints")
+const box = document.getElementById("rounds")
+const button = document.getElementById("start")
+const number = Math.floor(Math.random() * (101 - 1)) + 1
+function showCustom() {
+    if (custom.checked) {
+        customRounds.style.display = "inline-block"
+    } else {
+        customRounds.style.display = "none"
+    }
+}
+let i = 0
+function game() {
+    let rounds
+    let guess = guessed.value
+    if (four.checked) {
+        rounds = 4
+    } else if (five.checked) {
+        rounds = 5
+    } else if (six.checked) {
+        rounds = 6
+    } else {
+        rounds = customRounds.value
+    }
+    if (i >= rounds - 1) {
+        i++
+        button.disabled = true
+        box.style.color = "red"
+        button.style.color = "red"
+        hints.style.color = "red"
+    } else {
+        button.disabled = false
+        i++
+    }
+    box.innerHTML = `<h2>${i}/${rounds}</h2>`
+    if (guess == number) {
+        button.disabled = true
+        button.style.color = "#0d8a0d"
+        hints.style.color = "#0a570a"
+        box.style.color = "#0d8a0d"
+        hints.innerHTML += `<h3>Congratulatios! You won after ${i} ruounds. <a href="index.html">Play again!</a></h3>`
+    } else if (i >= rounds) {
+        hints.innerHTML += `<h3>Sorry, you lost. Your number was ${number}. <a href="index.html">Play again!</a></h3>`
+    } else if (guess > number) {
+        hints.innerHTML += `<h3>${i}. You need to guess lower than ${guess}.</h3>`
+    } else {
+        hints.innerHTML += `<h3>${i}. You need to guess higher than ${guess}.</h3>`
+    }
+}
